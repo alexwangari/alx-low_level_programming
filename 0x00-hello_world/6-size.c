6655c0174d996023512db3a7f495a5a1a71ab40b
alexwangari <alexwangari@gmail.com>
2022-10-14 19:56:43 -0700

    task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *
+ * * main - print a line of code using puts
+ *
+ * *
+ * * Return: 0
+ *
+ * */
+int main(void)
+{
+int a;
+long int b;
+long long int c;
+float d;
+char e;
+
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(e));
+
+return (0);
+
+}
