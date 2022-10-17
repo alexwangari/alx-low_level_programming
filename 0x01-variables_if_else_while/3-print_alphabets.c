aa56347c2a7989d0915e4e9e0f2eaf99fcd06bf0
alexwangari <alexwangari@gmail.com>
2022-10-17 05:42:18 -0700

    Tasks alphabet lower and upper

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,15 +3,15 @@
  * main - write aphabets in lowercase then upper case
  * Return: 0 when all is successful
 */
-int main (void)
+int main(void)
 {
 char alpha;
 
-for(alpha = 'a' ; alpha <= 'z' ; alpha++)
-putchar(alpha);	
-for(alpha = 'A' ; alpha <= 'Z' ; alpha++)
+for (alpha = 'a' ; alpha <= 'z' ; alpha++)
 putchar(alpha);
-putchar('\n');
+for (alpha = 'A' ; alpha <= 'Z' ; alpha++)
+putchar(alpha);
+putchar('\n')
 
 return (0);
 }
