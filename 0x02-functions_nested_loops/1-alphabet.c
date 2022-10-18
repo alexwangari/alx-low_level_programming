933678756e79480745f82e14dd0da60b246cea5f
alexwangari <alexwangari@gmail.com>
2022-10-18 04:28:02 -0700

    writting alphabets with custom command

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - writes alphabets a-z
+ *
+ * Return: void when successful
+ */
+void print_alphabet(void)
+{
+	int x;
+	for (x = 97 ; x <= 122 ; x++)
+	{
+	_putchar(x);
+	}
+
+	_putchar('\n');
+}
