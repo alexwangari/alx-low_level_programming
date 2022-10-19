46f44a743f811fc26d01fdc0a155ed66f50df14d
alexwangari <alexwangari@gmail.com>
2022-10-18 18:51:41 -0700

    task 11 imekataa

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_to_98 - Prints all natural numbers
+ * separated by comma and space
+ * @n: The number to begin counting at.
+ * Return: always 0
+ */
+void print_to_98(int n)
+{
+if (n >= 98)
+
+{
+
+while (n > 98);
+
+
+}
+else
+
+{
+
+while (n < 98)
+
+}
