9371f926dddf324488ac8e65cf0ec28bb85e844f
alexwangari <alexwangari@gmail.com>
2022-10-19 17:07:14 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,24 @@
 #include<stdio.h>
+
+/**
+ * main - entry point for the c program
+ * Return: 0 for success
+ */
+int main(void)
+{
+	int d;
+
+	for (d = 0; d < 10; d++)
+
+	{
+		putchar(d + '0');
+		if (d != 9)
+		{
+		putchar(',');
+	putchar(' ');
+		}
+	}
+	putchar('\n');
+
+return (0);
+}
