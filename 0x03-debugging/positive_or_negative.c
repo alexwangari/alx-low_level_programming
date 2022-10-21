ac4485895a17d1fabeeb20a26340108fe69dac26
alexwangari <alexwangari@gmail.com>
2022-10-21 13:20:50 +0000

    largest number

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <time.h>
+/**
+ *  * main - print if the number is positive,negative or 0
+ *   * Return: 0
+ *   */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n); /*display results if n is greater than 1*/
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);   /*display results if n is equal to zero*/
+	}
+	else
+
+	{
+		printf("%d is negative\n", n); /*display results if n is less than 0*/
+	}
+	return (0);
+}
