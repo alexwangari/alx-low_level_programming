31ef2daa37480afc882e7f82c13fa25ae31dbba9
alexwangari <alexwangari@gmail.com>
2022-10-21 05:18:14 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,11 +2,7 @@
 
 /**
  * main - tests function that prints if integer is positive or negative
-<<<<<<< HEAD
  * Return: 0
-=======
- *  Return: 0
->>>>>>> f874e709c1d3d8f4306bcdf69fab84f999ee08cf
  */
 
 int main(void)
