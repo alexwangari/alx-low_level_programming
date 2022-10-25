9b9a0c973a63569c88d40a69a651f8afd621464e
alexwangari <alexwangari@gmail.com>
2022-10-25 14:23:11 -0700

    task 01

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-/*
- *
+/**
  * reset_to_98 - reset's value of n to 98
  *
  * @n: variable to be reset
@@ -9,8 +8,8 @@
  */
 
 void reset_to_98(int *n)
-
-*n=98;
+{
+*n = 98;
 }
 
 
