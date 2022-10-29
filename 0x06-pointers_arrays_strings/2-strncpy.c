4f78b06ff79adffd68a634533daa04a6d70cbdb3
alexwangari <alexwangari@gmail.com>
2022-10-29 02:46:00 +0000

    more tasks

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - writes the character c to stdout
+ * @dest: pointer
+ * @src: pointer
+ * @n: integer
+ *
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+int j;
+
+j = 0;
+while (j < n && src[j] != '\0')
+{
+dest[j] = src[j];
+j++;
+}
+while (j < n)
+{
+dest[j] = '\0';
+j++;
+}
+
+return (dest);
+}
