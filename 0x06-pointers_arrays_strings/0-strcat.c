caff0829a73a9c7044f68267448b2ae1d0802989
alexwangari <alexwangari@gmail.com>
2022-10-28 19:26:49 -0700

    review

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * _strcat - writes the character c to stdout
- * @c: The character to print
+ * @dest: pointer
+ * @src: pointer
  *
  * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
