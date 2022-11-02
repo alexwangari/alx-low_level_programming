3d8d5fe3a351c1d17f2f4c99b9ef72dfcff8bcdf
alexwangari <alexwangari@gmail.com>
2022-11-02 16:40:22 +0000

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _memset - fill memory with a contant byte
+ *
+ * @s: pointer to memory area
+ * @n: the number of bytes to be filled
+ * @b: is a contant byte
+ *
+ * Return: a pointer to the memory area @s
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+
+unsigned int i;
+
+for(i = 0 ; i < n ; i++)
+{
+	s[i] = b;
+}
+return(s);
+}
