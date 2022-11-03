212f9d6d2495edf76d4cc7cd5a04c640fba1629a
alexwangari <alexwangari@gmail.com>
2022-11-03 17:52:12 +0000

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *
+ * _puts_recursion - display string
+ * @s: pointer to string
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+
+	if(*s == '\0')
+	{
+	_putchar('\n');
+	
+	return;
+	}
+	
+	_putchar(*s);
+
+	_puts_recursion(s + 1);
+
+}
