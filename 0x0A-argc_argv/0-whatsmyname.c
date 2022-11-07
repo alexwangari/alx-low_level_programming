d368188e3bc530949a01e8147cc10c1f29e3355e
alexwangari <alexwangari@gmail.com>
2022-11-07 06:24:22 -0800

    task 0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,18 @@
 #include "main.h"
-/*
- * main - the main function
+/**
+ * main - program to itself
+ *
+ * @argc: integer counter
+ * @argv: pointer to array of string
+ *
+ * Return: 0 when successful
  *
  */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
-	printf("%s\n",argv[0]);
+	printf("%s\n", argv[0]);
 
-return 0;
+return (0);
 }
