28df9a4b04542585a3f44ebf2b5a4e1d3bb3cd59
alexwangari <alexwangari@gmail.com>
2022-11-07 15:16:48 +0000

    Task 02

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - program to itself
+ *
+ * @argc: integer counter
+ * @argv: pointer to array of string
+ * @i: integer counter
+ *
+ * Return: 0 when successful
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	for (i = 0; i < argc; i++)
+	
+	printf("%s\n", argv[i]);
+	
+
+return (0);
+}
