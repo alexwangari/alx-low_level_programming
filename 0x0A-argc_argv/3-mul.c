bb56d080562fe6c52e035a6d98ff86990224a6ec
alexwangari <alexwangari@gmail.com>
2022-11-07 07:56:15 -0800

    Task 03

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -4,7 +4,6 @@
  *
  * @argc: integer counter
  * @argv: pointer to array of string
- * @i: integer counter
  *
  * Return: 0 when successful
  *
@@ -19,7 +18,7 @@ int main(int argc, char *argv[])
 	printf("Error\n");
 	return (1);
 	}
-			
+
 	n = atoi(argv[1]);
 
 	m = atoi(argv[2]);
