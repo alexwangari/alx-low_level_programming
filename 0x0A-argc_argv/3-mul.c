2e8bd373c633e041696e53c91e0c10d96afe71ab
alexwangari <alexwangari@gmail.com>
2022-11-07 15:47:25 +0000

    Task 03

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * main - program to itself
+ *
+ * @argc: integer counter
+ * @argv: pointer to array of string
+ * @i: integer counter
+ *
+ * Return: 0 when successful
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	int n, m, mul;
+
+	if (argc != 3)
+	{
+	printf("Error\n");
+	return (1);
+	}
+			
+	n = atoi(argv[1]);
+
+	m = atoi(argv[2]);
+
+	mul = n * m;
+
+	printf("%i\n", mul);
+
+return (0);
+}
